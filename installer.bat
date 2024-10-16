76e7c4e2d04b455b346435b57bf7c8b968442163
HeineCantor <carmine.palmese@gmail.com>
2024-10-16 14:37:35 +0200

    Update installer.bat for commons installation

--- installer.bat
+++ installer.bat
@@ -2,6 +2,14 @@
 
 GOTO :MAIN
 
+REM Installer function for commons
+:function0
+echo "Installing commons"
+cd "./commons"
+call mvn package || ( echo "Error in commons installation during mvn package" &&  exit /b 1 )
+exit /b 0
+
+
 REM Installer function for T1-G11
 :function1
 echo "Installing T1-G11"
@@ -96,7 +104,7 @@ docker volume create VolumeT8 || ( echo "Error in creating the volume T8" && exi
 REM Creazione della rete Docker 'global-network'
 docker network create global-network || ( echo "Error in creating the network global-network" && exit /b 1 )
 
-for /l %%i in (1,1,10) do (
+for /l %%i in (0,1,10) do (
 
    pushd .
    echo "Calling function # %%i:"
