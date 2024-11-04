af33a836bb5a37e97eb61e2eb738516f60539436
Macco117 <marcocimmino117@gmail.com>
2024-11-04 13:01:56 +0100

    Aggiunte classi test per Package Components

---
+++ T5-G2/t5/src/test/java/com/g2/Components/PageBuilderTest.java
@@ -0,0 +1,19 @@
+package com.g2.Components;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+import java.util.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.ui.Model;
+import com.g2.Interfaces.ServiceManager;
+import com.g2.t5.T5Application;
+
+@SpringBootTest(classes = T5Application.class)
+class PageBuilderTest {
+
+}
