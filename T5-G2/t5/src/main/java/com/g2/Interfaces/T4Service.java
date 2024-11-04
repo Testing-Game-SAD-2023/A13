b719ffc501f585efc461c6e1201d0884843127f6
PaoloR96 <134693980+PaoloR96@users.noreply.github.com>
2024-11-04 13:11:15 +0100

    test T4Service

--- T5-G2/t5/src/main/java/com/g2/Interfaces/T4Service.java
+++ T5-G2/t5/src/main/java/com/g2/Interfaces/T4Service.java
@@ -41,15 +41,6 @@ public class T4Service extends BaseService {
         // Inizializzazione del servizio base con RestTemplate e URL specificato
         super(restTemplate, BASE_URL);
 
-        // Registrazione dell'azione "getLevels" con una definizione specifica per
-        // questa azione
-        registerAction("getLevels", new ServiceActionDefinition(
-                // Definizione di un'operazione lambda che invoca il metodo getLevels con un
-                // parametro di tipo String
-                params -> getLevels((String) params[0]),
-                // L'azione è definita per accettare un parametro di tipo String
-                String.class));
-
         registerAction("CreateGame", new ServiceActionDefinition(
                 params -> CreateGame((String) params[0], (String) params[1], (String) params[2], (String) params[3],
                         (String) params[4]),
@@ -58,7 +49,7 @@ public class T4Service extends BaseService {
         registerAction("EndGame", new ServiceActionDefinition(
                 params -> EndGame((int) params[0], (String) params[1], (String) params[2], (int) params[3],
                         (Boolean) params[4]),
-                Integer.class, String.class, String.class, int.class, Boolean.class));
+                Integer.class, String.class, String.class, Integer.class, Boolean.class));
 
         registerAction("CreateRound", new ServiceActionDefinition(
                 params -> CreateRound((int) params[0], (String) params[1], (String) params[2]),
@@ -93,55 +84,6 @@ public class T4Service extends BaseService {
 
         String response = callRestGET("/robots", formData, String.class);
         return response;
-    }
-
-    /**
-     * Metodo che invia richieste per ottenere diversi "livelli" (levels) in
-     * base al nome della classe. Per ogni livello (da 0 a 10) e per ciascun
-     * tipo di robot ("randoop", "evosuite"), viene effettuata una chiamata REST
-     * per verificare la presenza di dati associati.
-     *
-     */
-    private List<String> getLevels(String className) {
-        // Inizializzazione di una lista per conservare i risultati
-        List<String> result = new ArrayList<>();
-
-        // Definizione dei tipi di robot che verranno utilizzati nella chiamata
-        List<String> robot_type = List.of("randoop", "evosuite");
-
-        // Iterazione su 11 livelli di difficoltà (da 0 a 10)
-        for (int i = 0; i < 11; i++) {
-            // Per ogni tipo di robot definito
-            for (String robot_string : robot_type) {
-                try {
-                    // Creazione di una mappa per i parametri del form da inviare nella richiesta
-                    // GET
-                    Map<String, String> formData = new HashMap<>();
-                    formData.put("testClassId", className); // Nome della classe
-                    formData.put("type", robot_string); // Tipo di robot
-                    formData.put("difficulty", String.valueOf(i)); // Livello di difficoltà corrente
-
-                    // Invio della richiesta GET tramite il servizio Rest, con i parametri e attesa
-                    // di una risposta di tipo String
-                    // <<<<<Nota:Non sappiamo il motivo per il quale è stato implementato in questo
-                    // modo>>>>
-                    String response = callRestGET("/robots", formData, String.class);
-
-                    // Se la risposta non è nulla, aggiungi il livello corrente alla lista dei
-                    // risultati
-                    if (response != null) {
-                        result.add(String.valueOf(i));
-                    }
-                } catch (Exception e) {
-                    // Gestione delle eccezioni, lancia un'eccezione personalizzata in caso di
-                    // errore
-                    break;
-                }
-            }
-        }
-
-        // Ritorna la lista dei livelli trovati
-        return result;
     }
 
     private int CreateGame(String Time, String difficulty, String name, String description, String username) {
@@ -229,4 +171,5 @@ public class T4Service extends BaseService {
         String respose = callRestPost(endpoint, formData, null, String.class);
         return respose;
     }
+
 }
