ad530cf3fd4644e0a733a5802ff5e09572006f3d
gabman78 <115697455+gabman78@users.noreply.github.com>
2024-11-28 14:13:59 +0100

    Modifiche profilo utente 1

--- T5-G2/t5/src/main/resources/static/t5/js/profile.js
+++ T5-G2/t5/src/main/resources/static/t5/js/profile.js
@@ -3,7 +3,7 @@ const getCookie = (name) => {
   const parts = value.split(`; ${name}=`);
   if (parts.length === 2) return parts.pop().split(';').shift();
   return null;
-}
+};
 
 const parseJwt = (token) => {
   try {
@@ -15,24 +15,31 @@ const parseJwt = (token) => {
 
 //TODO: questo è solo per DEMO, i dati del profilo devono essere contenuti in un model
 document.addEventListener("DOMContentLoaded", (e) => {
-      var userId = document.getElementById("user_id").innerHTML;
-      console.log(userId)
+  var userId = document.getElementById("user_id").innerHTML;
+  console.log(userId);
 
-      var userAPIString = '/students_list/{0}'.replace('{0}', userId);
+  var userAPIString = '/students_list/{0}'.replace('{0}', userId);
 
-      // Get user infos
-      $.ajax({
-        url: userAPIString,
-        type: 'GET',
-        timeout: 30000,
-        success: function (data, textStatus, xhr) {
-          document.getElementById("usernameField").innerText = data.email + " - " + data.name + " " + data.surname;
-
-          document.getElementById("studiesField").innerText = data.studies;
-        },
-        error: function (xhr, textStatus, errorThrown) {
-          console.error("Errore durante il recupero dei dati utente:", errorThrown);
-          swal("Errore","Errore durante il recupero dei dati utente","error");
-        }
-      });
+  // Get user infos
+  $.ajax({
+    url: userAPIString,
+    type: 'GET',
+    timeout: 30000,
+    success: function (data, textStatus, xhr) {
+      document.getElementById("usernameField").innerText = data.email + " - " + data.name + " " + data.surname;
+      document.getElementById("studiesField").innerText = data.studies;
+    },
+    error: function (xhr, textStatus, errorThrown) {
+      console.error("Errore durante il recupero dei dati utente:", errorThrown);
+      swal("Errore", "Errore durante il recupero dei dati utente", "error");
+    }
+  });
 });
+
+//Modifica (28/11) by GabMan 'Aggiunta della funzione showSection'
+function showSection(sectionId) {
+  // Rimuovi la classe "active" da tutte le sezioni
+  document.querySelectorAll('.section').forEach(section => section.classList.remove('active'));
+  // Aggiungi la classe "active" solo alla sezione selezionata
+  document.getElementById(sectionId).classList.add('active');
+}
