760523fb9d688aea7088ed0c9d3ff169977d336f
Giuseppe Castaldo <giuseppe.castaldo22@studenti.unina.it>
2024-11-30 10:50:37 +0100

    Modifiche UI Gateway
    Aggiunta al proxy delle rotte

--- T1-G11/applicazione/manvsclass/src/main/java/com/groom/manvsclass/service/AdminService.java
+++ T1-G11/applicazione/manvsclass/src/main/java/com/groom/manvsclass/service/AdminService.java
@@ -768,15 +768,11 @@ public class AdminService {
 
     /*Modifica 29/11/2024: Creazione metodi per reindirizzare alle view Teams e Assignments*/
     public ModelAndView showGestioneTeams(HttpServletRequest request, String jwt) {
-        if (jwtService.isJwtValid(jwt)) {
-            System.out.println("Show Gestione Team: gestione_teams.html");    
-            return new ModelAndView("redirect:/gestione_teams");}
         
         return new ModelAndView("redirect:/loginAdmin");
     }
 
     public ModelAndView showGestioneAssignments(HttpServletRequest request, String jwt) {
-        if (jwtService.isJwtValid(jwt)) return new ModelAndView("redirect:/gestione_assignments");
         return new ModelAndView("redirect:/loginAdmin");
     }
 
