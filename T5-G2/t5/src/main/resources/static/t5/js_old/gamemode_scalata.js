5938d75e2d61dbd0fc6b5af52f9f5995ccd7623e
Simone De Lucia <47185033+SimoneDeLucia@users.noreply.github.com>
2025-01-16 19:17:22 +0100

    Aggiunta metodo per recuperare nome classe robot e difficoltà

--- T5-G2/t5/src/main/resources/static/t5/js_old/gamemode_scalata.js
+++ T5-G2/t5/src/main/resources/static/t5/js_old/gamemode_scalata.js
@@ -159,13 +159,10 @@ function pressedSubmit() {
 
                         console.log("ScalataGameId: " + result.scalataGameId);
                         localStorage.setItem("scalataId", result.scalataGameId);
-                        localStorage.setItem("difficulty", 1);
-                        localStorage.setItem("robot", "evosuite");
                         localStorage.setItem("current_round_scalata", 1);
                         localStorage.setItem("scalata_name", selectedScalata);
-
                         retrieveScalata(selectedScalata)
-                            .then( data => {return createGame("evosuite", data[0].selectedClasses[0], 1, result.scalataGameId, username, "Scalata")})
+                            .then( data => {return createGame(localStorage.getItem("robot"), localStorage.getItem("classe"), localStorage.getItem("difficulty"), result.scalataGameId, username, "Scalata")})
                             .then( data => {
                                 return swal("Successo!", "La tua scelta è stata confermata, a breve verrai reindirizzato all'arena di gioco.", "success");
                             })
@@ -190,19 +187,49 @@ function pressedSubmit() {
     });
 }
 
-
+function getDifficulty(difficulty) {
+    switch (difficulty) {
+        case 'Beginner':
+            return 1;
+        case 'Intermediate':
+            return 2;
+        case 'Advanced':
+            return 3;
+        default:
+            return '';
+    }
+  }
 async function retrieveScalata(scalata) {
     return new Promise((resolve, reject) => { 
         $.ajax({
             url: `/retrieve_scalata/${scalata}`,
             type: 'GET',
             success: (data) => {
-                localStorage.setItem("SelectedScalata", selectedScalata);
+                // Estrarre i nomi delle classi dalla lista di oggetti
+                const classNames = data[0].selectedClasses.map(cls => cls.className);
+                const robots = data[0].selectedClasses.map(rbt => rbt.robot);
+                const difficulties = data[0].selectedClasses.map(dft => dft.difficulty)
+
+
+                // Salvare le informazioni necessarie in localStorage
+                localStorage.setItem("SelectedScalata", scalata);
                 localStorage.setItem("total_rounds_of_scalata", data[0].numberOfRounds);
-                localStorage.setItem("scalata_classes", JSON.stringify(data[0].selectedClasses));
-                localStorage.setItem("classe", data[0].selectedClasses[0]);
+                localStorage.setItem("scalata_classes", JSON.stringify(classNames));
+                localStorage.setItem("scalata_robots", JSON.stringify(robots));
+                localStorage.setItem("scalata_difficulties", JSON.stringify(difficulties));
+                localStorage.setItem("classe", classNames[0]); // Salva il nome della prima classe
+                
+                localStorage.setItem("difficulty", getDifficulty(data[0].selectedClasses[0].difficulty));
+                localStorage.setItem("robot", data[0].selectedClasses[0].robot);
+                
+                console.log(classNames[0]);
+                console.log(getDifficulty(data[0].selectedClasses[0].difficulty));
+                console.log(data[0].selectedClasses[0].robot);
+                
+                
                 resolve(data);
             },
+
             error: (error) => {
                 reject(error);
             }
