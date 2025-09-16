84e0a21684014197095285a0788ca1881a590c38
biagi <biagio.salzillo@studenti.unicampania.it>
2025-09-16 12:41:14 +0200

    add Leaderboard.html; add different languages;

--- T5-G2/t5/src/main/java/com/g2/Controllers/GuiController.java
+++ T5-G2/t5/src/main/java/com/g2/Controllers/GuiController.java
@@ -147,14 +147,14 @@ public class GuiController {
         return editor.handlePageRequest();
     }
 
-    @GetMapping("/leaderboard")
-    public String leaderboard(Model model, @CookieValue(name = "jwt", required = false) String jwt) {
-        PageBuilder leaderboard = new PageBuilder(serviceManager, "leaderboard", model);
-        ServiceObjectComponent listaUtenti = new ServiceObjectComponent(serviceManager, "listaPlayers", "T23", "GetUsers");
-        leaderboard.setObjectComponents(listaUtenti);
-        //leaderboard.SetAuth(jwt);
-        return leaderboard.handlePageRequest();
-    }
+//    @GetMapping("/leaderboard")
+//    public String leaderboard(Model model, @CookieValue(name = "jwt", required = false) String jwt) {
+//        PageBuilder leaderboard = new PageBuilder(serviceManager, "leaderboard", model);
+//        ServiceObjectComponent listaUtenti = new ServiceObjectComponent(serviceManager, "listaPlayers", "T23", "GetUsers");
+//        leaderboard.setObjectComponents(listaUtenti);
+//        //leaderboard.SetAuth(jwt);
+//        return leaderboard.handlePageRequest();
+//    }
     /* 
     @PostMapping("/save-scalata")
     public ResponseEntity<String> saveScalata(@RequestParam("playerID") int playerID,
