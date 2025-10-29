97802ef2c8c7cec48b411ab6b09d01a6eac2009a
biagi <biagio.salzillo@studenti.unicampania.it>
2025-10-29 10:18:36 +0100

    add annotation OpenAPI to PlayerController;

--- T23-G1/src/main/java/com/example/db_setup/controller/PlayerController.java
+++ T23-G1/src/main/java/com/example/db_setup/controller/PlayerController.java
@@ -2,10 +2,13 @@ package com.example.db_setup.controller;
 
 import com.example.db_setup.model.Player;
 import com.example.db_setup.model.dto.gamification.PlayerDTO;
-import com.example.db_setup.model.repository.PlayerRepository;
 import com.example.db_setup.service.PlayerService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -26,6 +29,20 @@ public class PlayerController {
         this.playerService = playerService;
     }
 
+    @Operation(
+            summary = "Get all players",
+            description = "Returns a list of all registered players"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "List of player returned successfully or empty if there are no players",
+                    content = @Content(
+                            mediaType = "application/json",
+                            array = @ArraySchema(schema = @Schema(implementation = PlayerDTO.class))
+                    )
+            )
+    })
     @GetMapping("/players")
     public ResponseEntity<List<PlayerDTO>> getAllPlayers() {
         return ResponseEntity.ok(playerService.getAllPlayers());
