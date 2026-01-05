5e451be0ea00e12590cadcfad5ff16cfe8d30a44
tonyemme0902-max <tonyemme0902@gmail.com>
2026-01-05 11:24:19 +0100

    CHECK COMMENTI PACKAGE MAPPER

--- T9/src/main/java/com/mapper/NotificationMapper.java
+++ T9/src/main/java/com/mapper/NotificationMapper.java
@@ -1,22 +1,20 @@
 package com.mapper;
 
 import com.model.Notification;
-// IMPORTA IL DTO DALLA LIBRERIA CONDIVISA
+
 import com.a13.notification.client.dto.NotificationDTO;
 import org.springframework.stereotype.Component;
 
 @Component
 public class NotificationMapper {
 
-    /**
-     * Converte un NotificationDTO (RabbitMQ) in una entity Notification (DB).
-     */
+    // Converte un NotificationDTO  in una entity Notification.
+
     public Notification toEntity(NotificationDTO dto) {
         if (dto == null) {
             return null;
         }
 
-        // Creando la nuova Entity, il suo costruttore fa già: this.createdAt = Instant.now();
         Notification notification = new Notification();
 
         // Campi principali (copiati dal DTO)
@@ -25,29 +23,24 @@ public class NotificationMapper {
         notification.setTitle(dto.getTitle());
         notification.setBody(dto.getBody());
 
-        // createdAt: NON viene più letto dal DTO.
-        // L'Entity userà il suo default (Instant.now()).
-
-        // isRead rimane al valore di default (false)
-
         return notification;
     }
 
-    /**
-     * Facoltativo: converte da entity a DTO.
-     */
+
+    // Converte una entity Notification in un NotificationDTO .
+
     public NotificationDTO toDto(Notification notification, String replyTo) {
         if (notification == null) {
             return null;
         }
 
         NotificationDTO dto = new NotificationDTO();
+
+        // Campi principali (copiati nel DTO)
         dto.setUserId(notification.getUserId());
         dto.setType(notification.getType());
         dto.setTitle(notification.getTitle());
         dto.setBody(notification.getBody());
-
-        // createdAt: NON viene copiato nel DTO perché il campo non esiste più nella libreria.
 
         dto.setReplyTo(replyTo);
 
--- T9/src/main/java/com/mapper/NotificationRestMapper.java
+++ T9/src/main/java/com/mapper/NotificationRestMapper.java
@@ -4,8 +4,12 @@ import com.model.Notification;
 import com.model.dto.NotificationRestDTO;
 import org.springframework.stereotype.Component;
 
+ //Mapper responsabile della conversione tra l'entity Notification e il  NotificationRestDTO
+
 @Component
 public class NotificationRestMapper {
+
+    // Converte una entity Notification in un NotificationRestDTO .
 
     public NotificationRestDTO toDTO(Notification n) {
         return new NotificationRestDTO(
